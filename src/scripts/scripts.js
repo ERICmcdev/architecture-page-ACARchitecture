a8e56a71d1ec8d6d4038f859213b611a09ee0e9e
Eric Macedo <98058748+ERICmcdev@users.noreply.github.com>
2022-02-10 22:27:25 -0300

    upload via FILES SRC

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,10 +1,39 @@
 var swiper = new Swiper(".mySwiper", {
-    pagination: {
-      el: ".swiper-pagination",
-      type: "progressbar",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const menuItems = document.querySelectorAll('.nav a[href^="#"]');
+
+menuItems.forEach(item => {
+  item.addEventListener('click', scrollToIdOnClick);
+  console.log(item)
+});
+
+function scrollToIdOnClick(event) {
+  event.preventDefault();
+  const to = getScrollTopByHref(event.currentTarget) - 80;
+  scrollToPosition(to);
+}
+
+function getScrollTopByHref(element) {
+  const id = element.getAttribute('href');
+  return document.querySelector(id).offsetTop;
+}
+
+function scrollToPosition(to) {
+  // Caso queira o nativo apenas
+  window.scroll({
+    top: to,
+    behavior: "smooth",
+  })
+  // smoothScrollTo(0, to);
+}
+
+
